32e25eda9a359ec9719b883d7d04d51e1a271e99
lizhe_git <lizhe_git@email>
2022-09-25 09:29:22 +0800

    li-github2 修改

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!你好啊github1111111111" );
+        System.out.println( "Hello World!你好啊github1111111111li-github2修改" );
+        System.out.println("li-github2修改");
     }
 }
